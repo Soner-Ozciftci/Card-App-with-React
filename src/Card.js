f84d36283654321c88383bdb3f1dc3d8ef5d19c4
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-14 18:40:38 +0200

    cardDescription

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Card.css";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
       <div className="cardBody">
-        <img src="" />
+        <img src={props.img} />
         <div className="about">
-          <h2 className="cardTitle"></h2>
-          <p className="cardDescription"></p>
+          <h2 className="cardTitle">{props.title}</h2>
+          <p className="cardDescription">{props.description}</p>
         </div>
       </div>
     </div>
