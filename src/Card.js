1103c452ff4a035d25d083428970ed30df809e0e
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-14 17:50:55 +0200

    card style add

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 export default function Card() {
   return (
-   <div className='card'>
-
-<div className='cardBody'>
-<img src='' />
-<div className='about'>
-    <h2 className='cardTitle'></h2>
-    <p className='cardDescription'></p>
-</div>
-</div>
-
-   </div>
-  )
+    <div className="card">
+      <div className="cardBody">
+        <img src="" />
+        <div className="about">
+          <h2 className="cardTitle"></h2>
+          <p className="cardDescription"></p>
+        </div>
+      </div>
+    </div>
+  );
 }
