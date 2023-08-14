6a8662d690b6e3aefe997f0eefbfcaa4baf7f748
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-14 17:33:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './Card.js';
+
+
+function App() {
+  return (
+    <div className="App">
+<div className='App'></div>
+<Card/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Card.css';
+
+export default function Card() {
+  return (
+   <div className='card'>
+
+<div className='cardBody'>
+<img src='' />
+<div className='about'>
+    <h2 className='cardTitle'></h2>
+    <p className='cardDescription'></p>
+</div>
+</div>
+
+   </div>
+  )
+}
